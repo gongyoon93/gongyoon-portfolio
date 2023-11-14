92927a407fb53c7fadf5ea84424582d6e9fae946
gongyoon93 <qkszn93@gmail.com>
2023-11-14 13:04:48 +0900

    second lottiefiles

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Animation from "./animation";
 
 const Hero = () => {
@@ -16,9 +17,11 @@ const Hero = () => {
           제시하여야 한다.
         </p>
         <div className="flex justify-center">
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            프로젝트 보러가기
-          </button>
+          <Link href="/projects">
+            <a className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              프로젝트 보러가기
+            </a>
+          </Link>
         </div>
       </div>
       <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
