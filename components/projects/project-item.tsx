39fd4b22b92366a5037264affaa32514ebff2bae
gongyoon93 <qkszn93@gmail.com>
2023-11-16 18:54:03 +0900

    retry

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -7,12 +7,13 @@ type InfoProps = {
 
 const ProjectItem = ({ data }: InfoProps) => {
   const cover = data.cover.file?.url || data.cover.external.url;
-  const title = data.properties["이름"].title[0].plain_text;
-  const description = data.properties["설명"].rich_text[0].plain_text;
-  const tags = data.properties["태그"].multi_select;
+  const title = data.properties["이름"].title[0]?.plain_text;
+  const description = data.properties["설명"].rich_text[0]?.plain_text;
+  const tags = data.properties["태그"]?.multi_select;
+  const url = data?.url;
   //   const github = data.properties.GitHub
-  const start = data.properties["날짜"].date.start?.toString();
-  const end = data.properties["날짜"].date.end?.toString();
+  const start = data.properties["날짜"].date?.start?.toString();
+  const end = data.properties["날짜"].date?.end?.toString();
 
   return (
     <div className="project-card">
@@ -29,10 +30,15 @@ const ProjectItem = ({ data }: InfoProps) => {
       <div className="p-4 flex flex-col">
         <h1 className="text-2xl font-bold">{title}</h1>
         <h3 className="mt-4 text-xl">{description}</h3>
-        <a href="">깃허브 바로가기</a>
-        <p className="my-1 ">
-          작업기간 : {start} {start && end !== undefined ? "~" : ""} {end}
-        </p>
+        {url && <a href={url}>URL 바로가기</a>}{" "}
+        {/* 링크는 새로운 창이 열리도록 수정  */}
+        {start || end ? (
+          <p className="my-1 ">
+            작업기간 : {start} {start && end ? "~" : ""} {end}
+          </p>
+        ) : (
+          ""
+        )}
         <div className="flex items-start mt-2">
           {tags.map((tag) => (
             <h1
